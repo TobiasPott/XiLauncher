2adb6c4df1398f21090a5c0c0040e0cfc6bf7db0
Tobias Pott <mailme@tobiaspott.de>
2025-08-11 11:22:41 +0200

    Removed unused Silk reference (future requirement for gamepad controls)

--- xilauncher/Input.cs
+++ xilauncher/Input.cs
@@ -1,37 +1,37 @@
-﻿using Silk.NET.XInput;
-using System.Diagnostics;
+﻿//using Silk.NET.XInput;
+//using System.Diagnostics;
 
-namespace xilauncher
-{
-    internal class Input
-    {
-        private static XInput? _input;
+//namespace xilauncher
+//{
+//    internal class Input
+//    {
+//        private static XInput? _input;
 
-        public Input()
-        {
-            if (_input == null)
-            {
-                _input = XInput.GetApi();
+//        public Input()
+//        {
+//            if (_input == null)
+//            {
+//                _input = XInput.GetApi();
 
-                for (uint i = 0; i < XInput.XuserMaxCount; i++)
-                {
-                    State state = new State();
-                    uint result = _input.GetState(i, ref state);
+//                for (uint i = 0; i < XInput.XuserMaxCount; i++)
+//                {
+//                    State state = new State();
+//                    uint result = _input.GetState(i, ref state);
 
-                    if (result == 0) // success
-                    {
-                        Debug.WriteLine($"Connected controller: {i}");
-                    }
-                    else
-                    {
-                        Debug.WriteLine($"No controller controller found: {i}");
-                    }
-                }
-            }
-            else
-            {
-                Debug.WriteLine($"XInput is already initialized. Updating controllers.");
-            }
-        }
-    }
-}
+//                    if (result == 0) // success
+//                    {
+//                        Debug.WriteLine($"Connected controller: {i}");
+//                    }
+//                    else
+//                    {
+//                        Debug.WriteLine($"No controller controller found: {i}");
+//                    }
+//                }
+//            }
+//            else
+//            {
+//                Debug.WriteLine($"XInput is already initialized. Updating controllers.");
+//            }
+//        }
+//    }
+//}
